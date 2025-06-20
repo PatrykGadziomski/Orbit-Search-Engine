0228dcef9da7e0508b7a016e19321d0a9d987ffc
Patryk Gadziomski <gadziomski.patryk@gmail.com>
2025-06-20 21:57:07 +0200

    fix config path

--- orbit_utils/orbit_config_loader.py
+++ orbit_utils/orbit_config_loader.py
@@ -4,12 +4,16 @@ Updated: 20.06.2025
 """
 
 import yaml
+import os
 
-def load_config(path: str="config.yaml") -> dict:
-    try:
-        with open(path, "r", encoding="utf-8") as file:
-            config = yaml.safe_load(file)
-        return config
-    except Exception as e:
-        print(f"Error loading the config file: {e}")
-        raise
+def load_config():
+    # Pfad zum Verzeichnis dieses Skripts (orbit_config_loader.py)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Ziel: config.yaml im Hauptverzeichnis
+    config_path = os.path.abspath(os.path.join(current_dir, "..", "config.yml"))
+
+    with open(config_path, "r", encoding="utf-8") as file:
+        config = yaml.safe_load(file)
+    
+    return config
